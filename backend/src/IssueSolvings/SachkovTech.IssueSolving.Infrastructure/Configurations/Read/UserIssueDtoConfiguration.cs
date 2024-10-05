4397452e3c8b8c78adc27e9f736bf362b8004985
brinersam <vladbudyachenko@mail.ru>
2024-10-05 03:25:48 +0300

    Add UserIssueDto class and its configuration

---
+++ backend/src/IssueSolvings/SachkovTech.IssueSolving.Infrastructure/Configurations/Read/UserIssueDtoConfiguration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SachkovTech.Core.Dtos;
+
+namespace SachkovTech.IssueSolving.Infrastructure.Configurations.Read
+{
+    public class UserIssueDtoConfiguration : IEntityTypeConfiguration<UserIssueDto>
+    {
+        public void Configure(EntityTypeBuilder<UserIssueDto> builder)
+        {
+            builder.ToTable("user_issues");
+
+            builder.HasKey(u => u.Id);
+        }
+    }
+}
